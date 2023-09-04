0e6e7b4c41b9c81c0b794d4d63ebf8475465bfdc
Nikolaus Lindinger <nikolaus.lindinger@gmx.at>
2023-09-04 15:43:29 +0200

    added python script that saves a matplotlib image

--- python/test.py
+++ python/test.py
@@ -1,18 +1,28 @@
+import os
 import pandas as pd
 import mysql.connector
+import matplotlib.pyplot as plt
 
 server_config = {
     'host': 'db',
     'port': 3306,
     'user': 'root',
-    'password': 'mysql',
-    'database': 'wca_db',
+    'password': os.environ["MYSQL_ROOT_PASSWORD"],
+    'database': os.environ["MYSQL_DATABASE"],
 }
-conn = mysql.connector.connect(**server_config)
-cursor = conn.cursor()
+connection = mysql.connector.connect(**server_config)
+cursor = connection.cursor()
 
-query = 'SHOW TABLES'
-df = pd.read_sql(query, conn)
-print(df)
+query = 'SELECT CAST(LEFT(id,4) AS UNSIGNED) AS year, COUNT(*) AS count FROM Persons GROUP BY year'
 
-print('db has been connected')
+df2 = pd.read_sql(query, connection)
+
+fig, ax = plt.subplots()
+fig.dpi = 100
+
+ax.bar(df2["year"],df2["count"])
+ax.set_title("New competitors by year")
+ax.set_xlabel("Year")
+ax.set_ylabel("New competitors")
+fig.savefig("bild.png", transparent = True)
+print("Saved plot")
