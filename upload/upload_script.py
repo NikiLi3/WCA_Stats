925bb0a5b15d672ae9b023f3e598c3bd39c787ab
Nikolaus Lindinger <nikolaus.lindinger@gmx.at>
2023-09-05 10:48:00 +0200

    added service to upload images to Github repository

---
+++ upload/upload_script.py
@@ -0,0 +1,17 @@
+import os
+from github import Github
+
+os.chdir('tmp/cache')
+
+g = Github(os.environ["GITHUB_ACCESS_TOKEN"])
+repo = g.get_repo('NikiLi3/NikiLi3.github.io')
+
+# TODO: create new directory depending on databased dump date
+
+folder = 'cache'
+
+for file_name in os.listdir():
+    with open(file_name, 'rb') as image:
+        f = image.read()
+        image_data = bytearray(f)
+    repo.create_file(folder + '/' + file_name, 'upload via API', bytes(image_data))
