d8d0ad844f82219603f32603efefedf4efa7669f
Nikolaus Lindinger <nikolaus.lindinger@gmx.at>
2023-09-05 15:50:30 +0200

    changed assets directory for Github pages

--- upload/upload_script.py
+++ upload/upload_script.py
@@ -7,11 +7,11 @@ g = Github(os.environ["GITHUB_ACCESS_TOKEN"])
 repo = g.get_repo('NikiLi3/WCA_Stats')
 
 # TODO: create new directory depending on databased dump date
-
-folder = 'images'
+#folder = 'images'
 
 for file_name in os.listdir():
+    print(file_name)
     with open(file_name, 'rb') as image:
         f = image.read()
         image_data = bytearray(f)
-    repo.create_file(folder + '/' + file_name, 'upload via API', bytes(image_data))
+    repo.create_file('docs/assets' + '/' + file_name, 'upload via API', bytes(image_data))
