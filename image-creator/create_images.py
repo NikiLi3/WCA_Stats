eb9b523e4b82baaa313a409188f938c34d32ac2d
Nikolaus Lindinger <nikolaus.lindinger@gmx.at>
2023-09-13 12:55:37 +0200

    improved generation of static website

--- image-creator/create_images.py
+++ image-creator/create_images.py
@@ -15,14 +15,22 @@ cursor = connection.cursor()
 
 count = 1
 
+os.chdir('images')
+if os.path.exists("stats.txt"):
+    os.remove("stats.txt")
 
-def savefig(figure):
+
+def savefig(title, figure):
     global count
-    figure.savefig("/tmp/cache/image" + str(count) + ".png", transparent=True)
+    image_file_name = "image" + str(count) + ".png"
+    figure.savefig("/images/" + image_file_name, transparent=True)
+    with open('stats.txt', 'a') as f:
+        f.write(title + '---' + image_file_name+'\n')
     count += 1
 
 
 # New competitors by year
+title = "New competitors by year"
 query = 'SELECT CAST(LEFT(id,4) AS UNSIGNED) AS year, COUNT(*) AS count FROM Persons GROUP BY year'
 
 df = pd.read_sql(query, connection)
@@ -33,9 +41,10 @@ fig.dpi = 150
 ax.bar(df["year"], df["count"])
 ax.set_xlabel("Year")
 ax.set_ylabel("New competitors")
-savefig(fig)
+savefig(title, fig)
 
 # Most common nationalities
+title = "ost common nationalities"
 query = 'SELECT Countries.name AS country, COUNT(DISTINCT(Persons.id)) AS count FROM Persons INNER JOIN Countries ON ' \
         'Persons.countryId=Countries.id GROUP BY country ORDER BY count DESC LIMIT 30'
 
@@ -48,9 +57,10 @@ ax.bar(df["country"], df["count"])
 ax.set_xlabel("Country")
 ax.set_ylabel("Number of people from that country")
 plt.xticks(rotation=90, ha="center")
-savefig(fig)
+savefig(title, fig)
 
 # Least common nationalities
+title = "Least common nationalities"
 query = 'SELECT Countries.name AS country, COUNT(DISTINCT(Persons.id)) AS count FROM Persons INNER JOIN Countries ON ' \
         'Persons.countryId=Countries.id GROUP BY country ORDER BY count ASC LIMIT 30'
 df = pd.read_sql(query, connection)
@@ -62,9 +72,10 @@ ax.bar(df["country"], df["count"])
 ax.set_xlabel("Country")
 ax.set_ylabel("Number of people from that country")
 plt.xticks(rotation=90, ha="center")
-savefig(fig)
+savefig(title, fig)
 
 # Region of origin distribution among WCA competitors
+title = "Region of origin distribution among WCA competitors"
 query = "SELECT con.name, COUNT(DISTINCT(p.id)) AS count FROM Persons AS p INNER JOIN Countries ON " \
         "p.countryId=Countries.id INNER JOIN Continents AS con ON Countries.continentId=con.id GROUP BY con.name " \
         "ORDER BY count DESC"
@@ -76,4 +87,7 @@ fig.dpi = 150
 
 ax.pie(df["count"], labels=df["name"], autopct='%1.1f%%', startangle=0,
        explode=[0.02 for _ in range(len(df) - 1)] + [0.3])
-savefig(fig)
+savefig(title, fig)
+
+with open("stats.txt") as f:
+    print(f.read())
--- upload/upload_script.py
+++ upload/upload_script.py
@@ -1,7 +1,9 @@
 import os
 from github import Github
 
-os.chdir('tmp/cache')
+os.chdir('images')
+if os.path.exists("index.md"):
+    os.remove("index.md")
 
 g = Github(os.environ["GITHUB_ACCESS_TOKEN"])
 repo = g.get_repo('NikiLi3/WCA_Stats')
@@ -9,9 +11,27 @@ repo = g.get_repo('NikiLi3/WCA_Stats')
 # TODO: create new directory depending on databased dump date
 #folder = 'images'
 
-for file_name in os.listdir():
-    print(file_name)
-    with open(file_name, 'rb') as image:
-        f = image.read()
-        image_data = bytearray(f)
-    repo.create_file('docs/assets' + '/' + file_name, 'upload via API', bytes(image_data))
+# for file_name in os.listdir():
+#     print(file_name)
+#     with open(file_name, 'rb') as image:
+#         f = image.read()
+#         image_data = bytearray(f)
+#     repo.create_file('docs/assets' + '/' + file_name, 'upload via API', bytes(image_data))
+
+with open('stats.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+    with open('index.md', 'w') as webpage:
+        for line in lines:
+            title, image = line.split('---')
+            webpage.writelines('## '+title+'\n\n')
+            webpage.writelines('![](assets/'+image+')\n\n')
+
+# Replace index.md file
+
+with open("index.md") as f:
+    contents = repo.get_contents("docs/index.md")
+    lines = f.readlines()
+    fileString = ''.join(lines)
+    repo.update_file(contents.path, "updated static webpage", fileString, contents.sha)
+
